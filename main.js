1eb57ab6c772c5073841866c63751fffa5744d94
Asahi Toledo <asahitld@hotmail.com>
2024-02-06 08:07:35 -0600

    main

---
+++ main.js
@@ -0,0 +1,10 @@
+///Creación de objetos, funciones y arreglos
+const persona = {
+nombre: "Daniela",
+caminar: function() {},
+hablar(){}
+};
+persona.hablar();
+persona['nombre'] = "Alejandra";
+const nombreobjetivo = "nombre";
+persona[nombreobjetivo.value] = "Patricia";
